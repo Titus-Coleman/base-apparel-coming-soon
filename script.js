a61dd20c579cd50d7ab4c7f0eb9a271f14a5a79e
Titus Coleman <dev@tituscoleman.com>
2022-12-11 15:50:58 -0500

    supabase working weak-security

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Create a single supabase client for interacting with your database
+const {createClient} = supabase;
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyZ3l0ZHhhcXNsYmtyaXpjeXFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA2MTcwMDUsImV4cCI6MTk4NjE5MzAwNX0.ybtD1ivAXolbFc2SHRMFKJ4FLmre1mwMlEf8J3CAwxI'
+
+supabase = createClient('https://hrgytdxaqslbkrizcyqc.supabase.co', SUPABASE_KEY);
+
 const isValidEmail = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
@@ -9,6 +15,8 @@ const emailInput = document.querySelector('input[name="email"]');
 let isFormValid = false;
 
 const validateInputs = () => {
+
+   
     if (!isValidEmail(emailInput.value)) {
         emailInput.classList.add("invalid");
         document.getElementById("error").classList.remove("hidden");
@@ -19,14 +27,27 @@ const validateInputs = () => {
         document.getElementById("error").classList.add("hidden");
         document.getElementById("warning--icon").classList.add("hidden");
         console.log('good email');
-        document.getElementById('form').reset();
+        return emailInput.value;
     }
 };
 
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
     e.preventDefault();
-    validateInputs();
+    if(validateInputs()){
+        const formInput = form.querySelectorAll('input')
+        let submission = {};
+        formInput.forEach(element => {
+            const {value, name} = element
+            if (value) {
+                submission[name] = value
+            }
+        })
+       
+        const {error} = await supabase.from('entries').insert([submission], {returning: 'minimal'})
+        console.log(submission)
+  
+    }
     
 });
 
